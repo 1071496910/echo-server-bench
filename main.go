e7286b804696fbd1f7fe7a17887d170a4e7cf846
hepanchen <hepanchen@meizu.com>
2019-07-17 11:28:19 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	concurrence int
+	throughput  int
+	server      string
+	port        string
+)
+
+func init() {
+	flag.IntVar(&concurrence, "c", 1, "concurrence num")
+	flag.IntVar(&throughput, "t", 1000, "total throughput")
+	flag.StringVar(&server, "s", "127.0.0.1", "server addr")
+	flag.StringVar(&port, "p", "80", "server port")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+
+	var n int64
+	var wg sync.WaitGroup
+	var readBuf = make([]byte, 5)
+
+	begin := time.Now()
+	wg.Add(concurrence)
+	for i := 0; i < concurrence; i++ {
+		go func() {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", server+":"+port)
+			if err != nil {
+				panic(err)
+			}
+			for {
+				if atomic.LoadInt64(&n) >= int64(throughput) {
+					return
+				}
+				_, err = conn.Write([]byte("hello"))
+				if err != nil {
+					panic(err)
+				}
+				_, err = conn.Read(readBuf)
+				if err != nil {
+					panic(err)
+				}
+				atomic.AddInt64(&n, 1)
+			}
+		}()
+	}
+	wg.Wait()
+	elapsed := time.Since(begin)
+	fmt.Println("Use time:", elapsed)
 }
